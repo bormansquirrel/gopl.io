01138726b4bfaa31b9c1b4fd5d276e347f2987e9
Mikel Ganuza <mikel@perkbox.co.uk>
2018-05-04 15:36:07 +0100

    Print the names of all files in which each duplicated line occurs

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -12,10 +12,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
+type match struct {
+	n int
+	file string
+}
+
 func main() {
-	counts := make(map[string]int)
+	counts := make(map[string]*match)
 	files := os.Args[1:]
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
@@ -31,16 +37,22 @@ func main() {
 		}
 	}
 	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+		if n.n > 1 {
+			fmt.Printf("%s\t%d\t%s\n", n.file, n.n, line)
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]*match) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		if counts[input.Text()] == nil {
+      counts[input.Text()] = &match{}
+		}
+		counts[input.Text()].n++
+		if ! strings.Contains(counts[input.Text()].file, f.Name()) {
+			counts[input.Text()].file += " " + f.Name()
+		}
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
