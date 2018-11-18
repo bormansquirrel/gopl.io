3a8cfe71309b3adb59c9cae165516187f89160f8
Mikel Ganuza <mikel.ganuza@perkbox.com>
2018-11-18 20:06:45 +0000

    add exercise 4.1

--- ch2/main.go
+++ ch2/main.go
@@ -4,21 +4,26 @@ package main
 import (
 	"fmt"
 
-	"gopl.io/ch2/tempconv"
+	//"gopl.io/ch2/tempconv"
+	"gopl.io/ch2/popcount"
 )
 
 func main() {
 
 	k := tempconv.Kelvin(3)
-	fmt.Printf("%s = %s\n", k, tempconv.KToC(k));
+	fmt.Printf("%s = %s\n", k, tempconv.KToC(k))
 	k = tempconv.Kelvin(3)
-	fmt.Printf("%s = %s\n", k, tempconv.KToF(k));
+	fmt.Printf("%s = %s\n", k, tempconv.KToF(k))
 	c := tempconv.Celsius(3)
-	fmt.Printf("%s = %s\n", c, tempconv.CToK(c));
+	fmt.Printf("%s = %s\n", c, tempconv.CToK(c))
 	c = tempconv.Celsius(3)
-	fmt.Printf("%s = %s\n", c, tempconv.CToF(c));
+	fmt.Printf("%s = %s\n", c, tempconv.CToF(c))
 	f := tempconv.Fahrenheit(3)
-	fmt.Printf("%s = %s\n", f, tempconv.FToC(f));
+	fmt.Printf("%s = %s\n", f, tempconv.FToC(f))
 	f = tempconv.Fahrenheit(3)
-	fmt.Printf("%s = %s\n", f, tempconv.FToK(f));
+	fmt.Printf("%s = %s\n", f, tempconv.FToK(f))
+
+	fmt.Printf("%d\n", popcount.PopCount(18446744073709551615))
+	fmt.Printf("%d\n", popcount.PopCount2(18446744073709551615))
+
 }
--- ch4/sha256/main.go
+++ ch4/sha256/main.go
@@ -11,15 +11,37 @@ import "fmt"
 //!+
 import "crypto/sha256"
 
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
 func main() {
-	c1 := sha256.Sum256([]byte("x"))
-	c2 := sha256.Sum256([]byte("X"))
-	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
+	c1 := sha256.Sum256([]byte("X"))
+	c2 := sha256.Sum256([]byte("x"))
+	fmt.Printf("%b\n%b\n%t\n%T\n", c1, c2, c1 == c2, c1)
 	// Output:
 	// 2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881
 	// 4b68ab3847feda7d6c62c1fbcbeebfa35eab7351ed5e78f4ddadea5df64b8015
 	// false
 	// [32]uint8
+
+	count := 0
+	for i, _ := range c1 {
+		for c1[i] > 0 { // until all bits are zero
+			if c1[i]&1 != c2[i]&1 { // check lower bit
+				count++
+			}
+			c1[i] >>= 1 // shift bits, removing lower bit
+			c2[i] >>= 1
+		}
+	}
+
+	fmt.Printf("Number of different bits: %d\n", count)
 }
 
 //!-
